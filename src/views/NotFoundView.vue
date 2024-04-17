34831fbb77264264805dfe2d9220263adf6ecad6
kemp.dewulf <kemp.dewulf@student.howest.be>
2024-04-17 11:57:36 +0200

    add 404 page

---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="not-found">
+    <h1>404</h1>
+    <p>Page not found</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NotFoundView'
+}
+</script>
+
+<style>
+.not-found {
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+</style>
