0a4c3a222cd5179b5a4cda6841391013341655d7
kemp.dewulf <kemp.dewulf@student.howest.be>
2024-04-17 21:04:09 +0200

    add navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,63 +1,74 @@
 <template>
-  <nav x-data="{ isOpen: false }" class="relative bg-white shadow dark:bg-gray-800">
-    <div class="container px-6 py-3 mx-auto md:flex">
-      <div class="flex items-center justify-between">
-        <a href="#">
-          <img class="w-auto h-6 sm:h-7" src="https://merakiui.com/images/full-logo.svg" alt="">
-        </a>
-
-        <!-- Mobile menu button -->
-        <div class="flex lg:hidden">
-          <button x-cloak @click="isOpen = !isOpen" type="button"
-                  class="text-gray-500 dark:text-gray-200 hover:text-gray-600 dark:hover:text-gray-400 focus:outline-none focus:text-gray-600 dark:focus:text-gray-400"
-                  aria-label="toggle menu">
-            <svg x-show="!isOpen" xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24"
-                 stroke="currentColor" stroke-width="2">
-              <path stroke-linecap="round" stroke-linejoin="round" d="M4 8h16M4 16h16"/>
+  <nav class="bg-header border-gray-200 sticky top-0">
+    <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+      <router-link to="/" class="flex items-center space-x-3 rtl:space-x-reverse">
+        <img src="/src/assets/images/logo.svg" class="h-8" alt="Hopix Logo" />
+        <span class="self-center text-2xl font-semibold whitespace-nowrap text-white">Hopix</span>
+      </router-link>
+      <div class="flex md:order-2">
+        <button type="button" data-collapse-toggle="navbar-search" aria-controls="navbar-search" aria-expanded="false" class="md:hidden text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 me-1">
+          <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+          </svg>
+          <span class="sr-only">Search</span>
+        </button>
+        <div class="relative hidden md:block">
+          <input type="text" id="search-navbar" class="block w-full p-2 ps-2 text-sm text-gray-900 border border-gray-300 rounded-full bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search...">
+          <div class="absolute inset-y-0 end-0 flex pe-5 items-center pointer-events-none">
+            <svg class="w-4 h-4  text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
             </svg>
-
-            <svg x-show="isOpen" xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24"
-                 stroke="currentColor" stroke-width="2">
-              <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12"/>
-            </svg>
-          </button>
+            <span class="sr-only">Search icon</span>
+          </div>
         </div>
+        <button data-collapse-toggle="navbar-search" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-search" aria-expanded="false">
+          <span class="sr-only">Open main menu</span>
+          <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
+          </svg>
+        </button>
       </div>
-
-      <!-- Mobile Menu open: "block", Menu closed: "hidden" -->
-      <div x-cloak :class="[isOpen ? 'translate-x-0 opacity-100 ' : 'opacity-0 -translate-x-full']"
-           class="absolute inset-x-0 z-20 w-full px-6 py-4 transition-all duration-300 ease-in-out bg-white dark:bg-gray-800 md:mt-0 md:p-0 md:top-0 md:relative md:opacity-100 md:translate-x-0 md:flex md:items-center md:justify-between">
-        <div class="flex flex-col px-2 -mx-4 md:flex-row md:mx-10 md:py-0">
-          <a href="#"
-             class="px-2.5 py-2 text-gray-700 transition-colors duration-300 transform rounded-lg dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700 md:mx-2">Home</a>
-          <a href="#"
-             class="px-2.5 py-2 text-gray-700 transition-colors duration-300 transform rounded-lg dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700 md:mx-2">About</a>
-          <a href="#"
-             class="px-2.5 py-2 text-gray-700 transition-colors duration-300 transform rounded-lg dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700 md:mx-2">Contact</a>
+      <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-search">
+        <div class="relative mt-3 md:hidden">
+          <input type="text" id="search-navbar" class="block w-full p-2 ps-2 text-sm text-gray-900 border border-gray-300 rounded-full bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search...">
+          <div class="absolute inset-y-0 end-0 flex pe-5 items-center pointer-events-none">
+            <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+            </svg>
+          </div>
         </div>
-
-        <div class="relative mt-4 md:mt-0">
-                <span class="absolute inset-y-0 left-0 flex items-center pl-3">
-                    <svg class="w-5 h-5 text-gray-400" viewBox="0 0 24 24" fill="none">
-                        <path
-                            d="M21 21L15 15M17 10C17 13.866 13.866 17 10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10Z"
-                            stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                            stroke-linejoin="round"></path>
-                    </svg>
-                </span>
-
-          <input type="text"
-                 class="w-full py-2 pl-10 pr-4 text-gray-700 bg-white border rounded-lg dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-opacity-40 focus:ring-blue-300"
-                 placeholder="Search">
-        </div>
+        <ul class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-header md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-header dark:h md:dark:bg-header dark:border-gray-700">
+          <li>
+            <router-link to="/" class="block py-2 px-3 text-white rounded md:p-0" exact-active-class="text-hopix-yellow">Home</router-link>
+          </li>
+          <li>
+            <router-link to="/beers" class="block py-2 px-3 text-white rounded md:p-0" exact-active-class="text-hopix-yellow">All beers</router-link>
+          </li>
+          <li>
+            <router-link to="/categories" class="block py-2 px-3 text-white rounded md:p-0" exact-active-class="text-hopix-yellow">Categories</router-link>
+          </li>
+          <li>
+            <router-link to="/top" class="block py-2 px-3 text-white rounded md:p-0" exact-active-class="text-hopix-yellow">Top 100</router-link>
+          </li>
+        </ul>
       </div>
     </div>
   </nav>
 </template>
 
 <script>
+import { onMounted } from 'vue'
+import { initFlowbite } from 'flowbite'
+onMounted(() => {
+  initFlowbite();
+})
 export default {
-  name: "Navbar"
+  name: "Navbar",
+  computed: {
+    isActive(routeName) {
+      return this.$route.name === routeName
+    }
+  }
 }
 </script>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,19 @@
+import flowbite from "flowbite/plugin";
+
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ['./index.html', './src/App.vue', './src/**/*.{vue,js,ts,jsx,tsx}'],
-    darkMode: 'false', // or 'media' or 'class'
+    content: ['./index.html', './src/App.vue', './src/**/*.{vue,js,ts,jsx,tsx}', "./node_modules/flowbite/**/*.js"],
+    darkMode: 'class',
     theme: {
+        colors: {
+            'header': '#282828',
+            'hopix-yellow': '#FFBD4A',
+        },
         extend: {},
     },
-    plugins: [],
+    plugins: [
+        flowbite
+    ],
+
 }
 
