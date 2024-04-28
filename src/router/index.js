345d8c5a2e0f3e5d72c7c83283d680d8e2f925c1
kemp.dewulf <kemp.dewulf@student.howest.be>
2024-04-28 22:05:17 +0200

    add route for beers page

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import {createRouter, createWebHistory} from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import NotFoundView from "@/views/NotFoundView.vue";
+import PageNotFoundView from "@/views/PageNotFoundView.vue";
+import BeerCatalogView from "@/views/BeerCatalogView.vue";
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -11,9 +12,14 @@ const router = createRouter({
             component: HomeView
         },
         {
+            path: '/beers',
+            name: 'beerCatalog',
+            component: BeerCatalogView
+        },
+        {
             path: '/:pathMatch(.*)*',
             name: 'not-found',
-            component: NotFoundView
+            component: PageNotFoundView
         }
     ],
     scrollBehavior(to, from, savedPosition) {
